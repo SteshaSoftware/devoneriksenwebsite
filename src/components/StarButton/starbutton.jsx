ddcf4ee6dfbeab8b261acdd06215c20856d59704
SteshaSoftware <stesha.software@gmail.com>
2023-10-25 13:23:25 -0400

    Stopgap solution to Safari issue

--- src/components/StarButton/starbutton.jsx
+++ src/components/StarButton/starbutton.jsx
@@ -5,8 +5,8 @@ const StarButton = () => {
             <a target='_blank' href={"https://read.bookfunnel.com/read/h6gjdipcpw"}>
                 <button class="btnn" type="button" style={{ marginTop: 25, width: "300px" }} >
                     <strong>Read 3-Chapter Preview</strong>
-                    <div id="container-stars">
-                        <div id="stars"></div>
+                    <div id="">
+                        <div id=""></div>
                     </div>
                     <div id="glow">
                         <div class="circle"></div>
