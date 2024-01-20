e7ada55420155dd544d0578db718609940221392
sarahallasa <105763320+sarahallasa@users.noreply.github.com>
2024-01-20 13:19:47 -0500

    Update exposition.jsx

--- src/writing/Craft/exposition.jsx
+++ src/writing/Craft/exposition.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const exposistion = {
+export const exposition = {
     title: `Why epic fantasies tend to get off to a slow start:`,
     content: `Epic fantasy is probably the form of fiction that requires the most exposition. These tend to be not just about a main character, although there may be one, but about the fate of entire worlds and peoples. 
     <br/><br/>
