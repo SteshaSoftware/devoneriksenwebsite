2091f51a8b4ce679a042aafa96bde712d1880e0c
sarahallasa <105763320+sarahallasa@users.noreply.github.com>
2024-02-13 12:00:38 -0500

    Update Biography.jsx

--- src/components/AuthorCard/Biography.jsx
+++ src/components/AuthorCard/Biography.jsx
@@ -1,6 +1,8 @@
 import { Box, Card, CardContent, CardMedia, Grid, Stack, Typography } from '@mui/material'
 import React from 'react'
 import photo from "../../images/Devon Author.jpg"
+import StarButton from '../StarButton/starbutton'
+import { FaArrowCircleRight } from 'react-icons/fa'
 
 const Biography = () => {
     return (
@@ -21,38 +23,47 @@ const Biography = () => {
                         ABOUT
                     </Typography>
                     <Typography component="div" variant="h5" mt={1.5} fontSize={{ xs: 28, sm: 28, md: 70, lg: 80 }} fontWeight={"bold"} fontFamily={"EB Garamond, serif"} color={"#FFFF"}>
-                    Devon Eriksen
+                        Devon Eriksen
                     </Typography>
-                    <Typography variant="subtitle1" color="white" component="div" sx={{ marginTop: { xs: 1.5 }, marginRight: { md: 15 }, fontSize:"17px", fontFamily:"Heebo, sans-serif" }}>
-                    Once upon a time, science fiction was about science and engineering. 
-                    Was about big ideas for how technology can transform the world. 
-                    Was about what a high-tech future might look like, and how it might feel 
-                    to be a real person living there. Was about our hopes and aspirations. 
-                    <br /><br />
-                    But something happened. Somewhere along the way, our fiction lost hope. 
-                    Began preaching our inevitable failure. Began sifting through the ashes 
-                    of endless visions of decay. Began lamenting instead of inspiring. Began, 
-                    above all, lecturing us for daring to have hopes, dreams, and desires, for 
-                    daring to think that the universe is a better place with humans in it than 
-                    without, that tomorrow might be better than today. 
-                    <br /><br />
-                    Well, some of us aren't interested in self-flagellation. And we're not 
-                    done with the big ideas yet. 
-                    <br /><br />
-                    I'm Devon Eriksen. And I'm writing the Orbital Space trilogy as a love 
-                    letter to every head-high pile of science fiction classics I ever hid in 
-                    the back of a public library with. Because I think the era of whining is 
-                    over, and humanity is going places again
-                    <br /><br /><hr /><br />
-                    Devon Eriksen is a life-long reader, with a particular fondness for 
-                    classics from the golden age of science fiction, such as "A Mote in God's 
-                    Eye" and "Ringworld." While he was dissuaded from becoming a writer in 
-                    his childhood, retirement from a career in engineering and the unwavering 
-                    support of his loved ones presented an opportunity to pursue his childhood 
-                    dream. Eriksen began writing stories with a single overarching goal: to 
-                    give the reader something to love. 
-                    <br /><br />
-                    And he hasn't looked back since.
+                    <Typography variant="subtitle1" color="white" component="div" sx={{ marginTop: { xs: 1.5 }, marginRight: { md: 15 }, fontSize: "17px", fontFamily: "Heebo, sans-serif" }}>
+                        Once upon a time, science fiction was about science and engineering.
+                        Was about big ideas for how technology can transform the world.
+                        Was about what a high-tech future might look like, and how it might feel
+                        to be a real person living there. Was about our hopes and aspirations.
+                        <br /><br />
+                        But something happened. Somewhere along the way, our fiction lost hope.
+                        Began preaching our inevitable failure. Began sifting through the ashes
+                        of endless visions of decay. Began lamenting instead of inspiring. Began,
+                        above all, lecturing us for daring to have hopes, dreams, and desires, for
+                        daring to think that the universe is a better place with humans in it than
+                        without, that tomorrow might be better than today.
+                        <br /><br />
+                        Well, some of us aren't interested in self-flagellation. And we're not
+                        done with the big ideas yet.
+                        <br /><br />
+                        I'm Devon Eriksen. And I'm writing the Orbital Space trilogy as a love
+                        letter to every head-high pile of science fiction classics I ever hid in
+                        the back of a public library with. Because I think the era of whining is
+                        over, and humanity is going places again
+                        <br />
+                    </Typography>
+                    <Box sx={{ display: "flex", alignItems: { xs: "center" }, flexDirection: { xs: "column", md: "row" }, justifyContent: { xs: "center", md: "start" }, marginBottom: '20px' }}>
+                    <StarButton GlowTxt="Read 3-Chapter Preview" LinkGlow="https://read.bookfunnel.com/read/h6gjdipcpwnp" />
+                    </Box>
+                    <a href="https://www.amazon.com/Theft-Fire-Orbital-Space-1/dp/1962514021/?tag=devoneriksen-20" target="_blank" rel="noopener noreferrer" onClick={() => window.sa_event('AboutBuy')}>
+                        <button class="buy-now-button">Click to Buy Theft of Fire <FaArrowCircleRight /> </button>
+                    </a>
+                    <Typography variant="subtitle1" color="white" component="div" sx={{ marginTop: { xs: 1.5 }, marginRight: { md: 15 }, fontSize: "17px", fontFamily: "Heebo, sans-serif" }}>
+                        <hr /><br />
+                        Devon Eriksen is a life-long reader, with a particular fondness for
+                        classics from the golden age of science fiction, such as "A Mote in God's
+                        Eye" and "Ringworld." While he was dissuaded from becoming a writer in
+                        his childhood, retirement from a career in engineering and the unwavering
+                        support of his loved ones presented an opportunity to pursue his childhood
+                        dream. Eriksen began writing stories with a single overarching goal: to
+                        give the reader something to love.
+                        <br /><br />
+                        And he hasn't looked back since.
                     </Typography>
 
                 </CardContent>
