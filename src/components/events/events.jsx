4b9624cf8c43e3cce10a7660a7497fb70ff0cd24
sarahallasa <105763320+sarahallasa@users.noreply.github.com>
2024-03-09 22:38:17 -0500

    Update events.jsx

--- src/components/events/events.jsx
+++ src/components/events/events.jsx
@@ -11,16 +11,8 @@ const Eventss = () => {
             </Typography>
             <Grid container sx={{ marginTop: 1, marginBottom: 30 }} justifyContent={"center"} spacing={4}>
                 <Grid item xs={12} md={6}>
-                    <a href="https://www.confinementcon.org/" onClick={() => window.sa_event('event')}>
-                        <div data-aos="zoom-out-right" className='p-3 rounded-xl event-card   '>
-                            <Typography variant='body1' sx={{ color: "#b79cff", fontSize: 20, fontWeight: "bold" }}>03/01 - 03/03</Typography>
-                            <Typography variant='small' sx={{ color: "#ff9b02", fontSize: 20 }}>ConFinement Lebanon, TN</Typography>
-                        </div>
-                    </a>
-                </Grid>
-                <Grid item xs={12} md={6}>
                     <a href="https://www.libertycon.org/" onClick={() => window.sa_event('event')}>
-                        <div data-aos="zoom-out-left" className='p-3 rounded-xl event-card   '>
+                        <div data-aos="zoom-out-right" className='p-3 rounded-xl event-card   '>
                             <Typography variant='body1' sx={{ color: "#b79cff", fontSize: 20, fontWeight: "bold" }}>06/21 - 06/23 </Typography>
                             <Typography variant='small' sx={{ color: "#ff9b02", fontSize: 20 }}>LiberyCon Chattanooga, TN</Typography>
                         </div>
@@ -28,7 +20,7 @@ const Eventss = () => {
                 </Grid>
                 <Grid item xs={12} md={6}>
                     <a href="https://writerscantina.org/" onClick={() => window.sa_event('event')}>
-                        <div data-aos="zoom-out-right" className='p-3 rounded-xl event-card '>
+                        <div data-aos="zoom-out-left" className='p-3 rounded-xl event-card '>
                             <Typography variant='body1' sx={{ color: "#b79cff", fontSize: 20, fontWeight: "bold" }}>06/28 - 06/29</Typography>
                             <Typography variant='small' sx={{ color: "#ff9b02", fontSize: 20 }}>Writer’s Cantina West Valley City, UT</Typography>
                         </div>
@@ -36,7 +28,7 @@ const Eventss = () => {
                 </Grid>
                 <Grid item xs={12} md={6}>
                     <a href="https://www.entertheimaginarium.com/" onClick={() => window.sa_event('event')}>
-                        <div data-aos="zoom-out-left" className='p-3 rounded-xl event-card   '>
+                        <div data-aos="zoom-out-right" className='p-3 rounded-xl event-card   '>
                             <Typography variant='body1' sx={{ color: "#b79cff", fontSize: 20, fontWeight: "bold" }}>07/19 - 07/21</Typography>
                             <Typography variant='small' sx={{ color: "#ff9b02", fontSize: 20 }}>Imaginarum Louisville, KY</Typography>
                         </div>
