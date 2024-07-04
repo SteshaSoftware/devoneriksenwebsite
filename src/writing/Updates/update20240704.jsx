b5e66ac8809a1ea0ae3842ebe9ff023b3f015b07
sarahallasa <105763320+sarahallasa@users.noreply.github.com>
2024-07-04 13:03:09 -0400

    Update update20240704.jsx

---
+++ src/writing/Updates/update20240704.jsx
@@ -0,0 +1,38 @@
+export const update20240704 = {
+    title: `Box of Trouble Update`,
+    content:  `1,592 words last night and this morning. 
+    <br/><br/>
+    One of the most important things about getting started as a writer is finding your process. <b>No one can tell you how to write, they can only tell you how they write.</b> Writing is the process of getting your thoughts out of your head and into other people's, so there are as many ways of figuring out what to write as there are heads. 
+    <br/><br/>
+    It's highly idiosyncratic, and one of the worst things you can do to your writing productivity is slavishly try to follow the advice of someone else, just because he is a successful writer. He's working out of his head, you're working out of yours. 
+    <br/><br/>
+    Listen to advice, but if it doesn't immediately click with your process, and your head, discard it. 
+    <br/><br/>
+    My process involves a great deal of planning, writing outlines and notes, and slowly discarding story ideas for better ones. The actual text tends on happen all in a rush at the end, when I've solved every last detail in my head. 
+    <br/><br/>
+    Every time I've tried to follow the standard writing advice, of "just write, then fix it later", or "silence your inner editor", my efforts have bogged down in a tangled mess. 
+    <br/><br/>
+    If authors hadn't tried to tell me that, you could have been reading Devon Eriksen novels decades earlier. I was completely stalled until I discarded that piece of advice. 
+    <br/><br/>
+    There are plenty of you out there, working on your own pieces of art, and finding it a struggle to get anywhere. 
+    <br/><br/>
+    Some of you I've talked to about this. 
+    <br/><br/>
+    Some of you get on twitter and complain about writer's block and being stuck, and then don't call me when you have my fucking phone number. You know who you are. 
+    <br/><br/>
+    Point is, there are certain fields where there's one right way to do it, like mathematics or physics, and you should listen to experienced dudes. 
+    <br/><br/>
+    There are others that are highly individual, encompassing many paths to many goals, and experts should be told to pound sand the moment they become dogmatic. Storytelling is one of these. 
+    <br/><br/>
+    If you're blocked, if you're struggling, if you can't seem to finish anything, if you hate what you write, you are working against the skill set and inclinations, not with them. 
+    <br/><br/>
+    Not everyone is capable of being a good writer. Not everyone has that spark of talent that makes their stories reach out and grab people. But there are many who have it within them, unseen, because they haven't found their process yet. 
+    <br/><br/>
+    This is mine. 
+    <br/><br/>
+    Your patience will be rewarded.`,
+    link: 'https://x.com/Devon_Eriksen_/status/1808882637223718916',
+    date: '2024-07-04',
+    type: 'twitter',
+    tag: 'updates',
+    };
