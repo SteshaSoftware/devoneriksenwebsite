2402509df4d3de1d2ca0a6020bdf41c9b3588c90
sarahallasa <105763320+sarahallasa@users.noreply.github.com>
2024-07-06 21:39:47 -0400

    writing update

---
+++ src/writing/Updates/update20240706.jsx
@@ -0,0 +1,23 @@
+export const update20240706 = {
+    title: `Box of Trouble Update: 75k words`,
+    content:  `Box of Trouble: 
+<br/><br/>
+1,797 words today so far, 75K words total. Revised structure summary finished. 
+<br/><br/>
+Some of that word count is outlines and structure, but literal text should emerge faster now. 
+<br/><br/>
+127 days until November 11. Possibly I can finish the manuscript by then, but probably not with enough time to spare for post-production. 
+<br/><br/>
+Cover art, editing, beta reads, revisions, layout, marketing copy, release logistics... lot of work. 
+<br/><br/>
+Targeting the beginning of next year gives me 51 more days. Targeting Christmas, which has obvious advantages, would give me 44 days. 
+<br/><br/>
+Tricky. We'll see. 
+<br/><br/>
+Gotta get my 1000 words every day without fail.
+    I'll get faster at this with time and repetition, not slower. But right now all I can do is place one foot in front of the other, knowing I will eventually reach Samarkand, because the road is only so long.`,
+    link: 'https://x.com/Devon_Eriksen_/status/1809654305634353339',
+    date: '2024-07-06',
+    type: 'twitter',
+    tag: 'updates',
+    };
