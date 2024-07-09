6009a834177a12f6ece0a2f891e25e276f583aa1
sarahallasa <105763320+sarahallasa@users.noreply.github.com>
2024-07-08 21:19:12 -0400

    Dragonaward nominations

--- src/components/Highlights/highlightlist.jsx
+++ src/components/Highlights/highlightlist.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import Audio from '../../images/AudioSignup.png'
 import DisSciFi from '../../images/DisSciFi5.png'
 import BoTUpdate from '../../images/BoTUpdate.png'
-
+import DragonAward from '../../images/DragonAward.png'
 
 export const HighlightList = {
     HL_List: [
+        {
+            Pic: DragonAward,
+            Picalt: 'Dragon Award Nomination',
+            Link: 'https://application.dragoncon.net/dc_fan_awards_nominations.php', 
+            Click: 'HL_DragonAward'
+        },
         {
             Pic: DisSciFi,
             Picalt: 'Discover Sci Fi #5',
