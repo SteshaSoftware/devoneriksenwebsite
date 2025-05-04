2a66593f4adbda79fed299af61fd0ceb75b3b65d
sarahallasa <105763320+sarahallasa@users.noreply.github.com>
2025-05-03 20:30:40 -0400

    bug fix

---
+++ src/writing/Updates/update20250501.jsx
@@ -0,0 +1,133 @@
+export const update20250501 = {
+    title: `The Publishing Process and Box of Trouble Update`,
+    content:  `Question: "Do you have a ballpark date for the publication of the sequel to, 'Theft of Fire'"? 
+<br/><br/>
+Answer: There's no simple one sentence answer to that. 
+<br/><br/>
+But I can pull back the curtain and shed some light on the process of how a novel goes from a collection of ideas to a book you can hold in your hand, and give you a better idea of where the sequel is at in this journey. 
+<br/><br/>
+Completing a manuscript, which can be read all the way through as a complete story, is the biggest step in producing a novel, requiring the most time, but it's only the first of many. 
+<br/><br/>
+Once a manuscript is complete, it must be edited — refined, sanded down, and the rough spots removed. 
+<br/><br/>
+For some authors, generally the ones who produce manuscripts quickly, this is a lot of work and takes a lot of time. 
+<br/><br/>
+For others, who write more slowly, the initial manuscript is more polished and closer to complete. 
+<br/><br/>
+Regardless, the editing process consists of four parts. Not all authors use all of them, but most use more than one, and some are pretty much mandatory for any serious author.  
+<br/><br/>
+Developmental editing: This means partial rewrites of the overall story structure to fix plot holes and continuity errors, and improve pacing and flow. 
+<br/><br/>
+Line editing: This is the process of fixing and improving the line by line writing style and use of language. 
+<br/><br/>
+Copy editing: This step focuses on the technical aspects of language, such as punctuation, capitalization, spelling, grammatical rules, and so forth. Obviously, it must be done last, and it can be complicated by the style of the author. Sometimes we deliberately break  grammatical rules in order to create a particular effect in the story. 
+<br/><br/>
+Beta reading: While other types of editing can be done by the author himself, or with the assistance of a professional editor, beta reading is the process of getting feedback from non-professional readers on their experiences and reactions while reading the manuscript, then editing based on a professional understanding of that feedback. 
+<br/><br/>
+As you can imagine, most of these steps can't be done in parallel, as there's absolutely no point in copy editing a story you are still performing post-beta edits on. The lines will change. 
+<br/><br/>
+But once these steps are complete, you still don't have a book. 
+<br/><br/>
+You have a text. 
+<br/><br/>
+The text needs to be proof read. This is a final pass, typically multiple passes, through the whole thing to find grammar and spelling errors, doubled or omitted words, and so forth, which copy editing missed. 
+<br/><br/>
+There will always be some. In fact, proof reading will usually miss one or two. In the future, AI tools may take over this process, which would probably improve it a great deal.  
+<br/><br/>
+Novels aren't typically published on webpages, so next you need to fit that text into its publication format. This is called layout, and it's typically done at least twice, often three times, for paperback, hardback, and ebook versions. This process also involves fitting in any internal art you commissioned, setting up chapter and section header styles, justifying text, choosing hardcopy dimensions and fonts, and finalizing page counts. 
+<br/><br/>
+Then you need cover art, which means working with an artist, usually over the space of several weeks. Good cover artists will read your book front to back. 
+<br/><br/>
+Once cover art is settled on and in process, you need cover design, the process of fitting that art on the cover, and integrating with the title, author name, whatever other text you have, including your back cover or flyleaf text which you also need to write. 
+<br/><br/>
+Writing back cover text is non-trivial. Try working on something for a year, then summing in up in six to eight exciting sentences, and you'll see what I mean. 
+<br/><br/>
+Once you have all that, there are many design decisions to make still, because you need prelaunch publicity, sales pages on Amazon and your other platforms, and so on. 
+<br/><br/>
+<b>Then, finally, you can release a book.</b> 
+<br/><br/>
+Okay, was that a lot of reading? Were you eager for me to just get to the point already? 
+<br/><br/>
+That kind of IS the point. If it was a slog to read all that, imagine how much more time and effort it takes to actually do it all. 
+<br/><br/>
+The point is, while an author might be able to give you some sort of guess about when he'll finish his manuscript, he has to do a lot of work with a lot of other people to turn that manuscript into a book. 
+<br/><br/>
+And that adds uncertainty. 
+<br/><br/>
+For traditionally published authors, some parts of this process are done by the publisher, who typically has a lot of practice and a smooth workflow. This can make it run faster, sometimes, but the results are typically worse because the author doesn't have the same level of creative control. 
+<br/><br/>
+I'm doing most everything myself, with the assistance of a handful of people who learned the process along with me. 
+<br/><br/>
+So, how long does a novel take me? 
+<br/><br/>
+Well, the initial manuscript for Theft of Fire, from concept to draft, took about nine months. However, it was not completed as a book and published for a over a year after that. 
+<br/><br/>
+Why? 
+<br/><br/>
+Well, a lot of that time was wasted. 
+<br/><br/>
+Time wasted looking for a traditional agent and publisher, before I decided to control my own fate. 
+<br/><br/>
+Time wasted hiring and firing my first editor, who misrepresented herself, claiming she had worked for Brandon Sanderson and Dan Wells, but in truth she had only been a personal assistant to the latter, and not served in any editorial capacity. 
+<br/><br/>
+Time wasted hiring and firing my first cover artist, who made excellent sketches, but his final product looked as if it were drawn by a drunk twelve year old. 
+<br/><br/>
+Time wasted on beta readers who didn't finish the book for months, or who simply never opened it at all. A first-time author is an unknown, and readers don't have a lot of faith in internet randos. 
+<br/><br/>
+Time wasted, most of all, in figuring all this stuff out. 
+<br/><br/>
+So, post-production for Box of Trouble will be a great deal faster, but... how much? This remains to be seen. 
+<br/><br/>
+Another factor is common to all authors in their second novel.  I call it the "sophomore slump", or the "sophomore squeeze". Many people have noticed that a new author's second novel is often significantly delayed, or significantly worse than his first, or both. 
+<br/><br/>
+This is real, and it's because the transition from hobbyist to professional is a difficult one. When you are working on your first novel, you have no audience, no public, no deadline, no expectations. No one knows who you are, or cares. 
+<br/><br/>
+This can create difficulties of its own, and confidence and emotional support is needed, but mostly you can push forward without any distractions. 
+<br/><br/>
+Once you publish a novel, however, you're a public figure. You have an audience. And this changes the equation. There are expectations now. People are watching. <b>And the more of a hit your first novel is, the greater the pressure on you when you write the second.</b> 
+<br/><br/>
+Not only that, you now must divide your time between publicity and the sequel. 
+<br/><br/>
+Somewhere in tradpub, there may exist quasi-mythical beings known as the "marketing team" and the "marketing budget", but you will not see them, because they function as a sort of unicorn in reverse, shunning the inexperienced and gravitating to those with the most experience, history, and clout. 
+<br/><br/>
+So you have to get yourself out there, appearing on podcasts and at conventions, writing marketing copy, working on your social media presence. And this, too, becomes more demanding the more successful you are. 
+<br/><br/>
+It's easy to say "just ignore that and write", but if you don't, your book won't sell, won't be read, and won't generate buzz for the next one. 
+<br/><br/>
+You have to do both, and learn, somehow, to shift gears between them. 
+<br/><br/>
+And then you have to figure out how to make an audiobook without a publishing house to help you. Which means more work to build a kickstarter campaign, and then to hire actors, direct the recording, find the right sound engineer, work with him to design everything in the same way that you would lay out a print book...
+<br/><br/>
+And that's around the time your wife collapses on the floor because there's a tumor bleeding into her brain, and suddenly, in the middle of that kickstarter campaign, you're facing a multi-year battle with stage 4 metastatic cancer. One that you might not win. 
+<br/><br/>
+Although, to be honest, I'm not entirely sure that last one is a standard part of the process. Could be just me. Perhaps some other authors can clarify this in the comments section? 
+<br/><br/>
+So... after having read all that, are you tired yet? 
+<br/><br/>
+Imagine how I feel. 
+<br/><br/>
+If you made it through all that, then perhaps you can understand why I cannot easily give you a release date, especially one that can be interpreted as a promise. 
+<br/><br/>
+Here's what I can tell you. 
+<br/><br/>
+There is hope. I have been greatly slowed, but not derailed. Even in the darkest moments, driving hundreds of miles every other day to the south's premier cancer treatment center, I did what I could, little as that was. 
+<br/><br/>
+I have 133,000 words written, a mix of literal finished text and highly detailed summary with literal dialogue, blocking, and beats. 
+<br/><br/>
+It will take me more time than I would like to finish that manuscript, but I am getting my daily 1000 words more often than not, and I am starting to be able to make out the finish line, distantly through the haze. 
+<br/><br/>
+Once that manuscript is there, I can move faster than last time. I have team members in place who know their jobs, I have the leverage to tell beta readers they have two weeks to finish, and we know all the steps we need to make. 
+<br/><br/>
+Best of all, I can promise you one thing... there will be no sophomore slump. I've learned from Theft of Fire, and from the copious feedback, and I believe I have improved as a writer. 
+<br/><br/>
+Box of Trouble may not be to everyone's taste, it certainly goes to some dark places, and is not suitable for small children, but if you liked Theft of Fire, I think you will not be disappointed. 
+<br/><br/>
+I simply have to ask for your patience. I take my responsibilities seriously, but this last year has been very hard.
+<br/><br/>
+I'm working on it.
+`,
+    link: 'https://x.com/Devon_Eriksen_/status/1917989951200711167',
+    date: '2025-05-01',
+    type: 'twitter',
+    tag: 'updates',
+    };
